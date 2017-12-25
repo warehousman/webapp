f443213fbfd036cff92b4d1422f2c95b04e37f3e
warehousman <warehousman@gmail.com>
2017-12-25 21:58:25 +0200

    add test

---
+++ project/tests/test.py
@@ -0,0 +1,39 @@
+import unittest
+
+from project import app
+
+
+class ProjectTests(unittest.TestCase):
+
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+        self.assertEquals(app.debug, False)
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    ########################
+    #### helper methods ####
+    ########################
+
+    ###############
+    #### tests ####
+    ###############
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertIn(b'Welcome to Popovs Family Portal!', response.data)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
